acfc35a37c67213bc5d9223902d23866b1800407
Kyle <kylekratt@gmail.com>
2018-11-08 14:58:20 -0500

    Changed letter spacing for aesthetic appeal, fixed function timing so that the win message appears after the word is revealed.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var wordGame ={
+var wordGame = {
     wordList: [
         'Bayonetta',
         'Bowser',
@@ -73,66 +73,87 @@ var wordGame ={
         'Zelda',
         'Zero Suit Samus'
     ],
-    guesses : 10,
-    wins : 0,
-    guessedLetters : [],
-    word : "",
-    gameword : "",
-    hiddenWord : [],
-    hiddenword: function(){
-        this.hiddenWord = [];
-        for(var i=0;i<this.word.length;i++){
-            if(/[a-z]/i.test(this.word.charAt(i)))
-            this.hiddenWord[i]="_";
+    guesses: 6,
+    wins: 0,
+    guessedLetters: [],
+    word: "",
+    gameword: "",
+    hiddenWord: [],
+    hiddenword: function () {
+        for (var i = 0; i < this.word.length; i++) {
+            if (/[a-z]/i.test(this.word.charAt(i)))
+                this.hiddenWord[i] = "_";
             else
-            this.hiddenWord[i]=this.word.charAt(i);
-            console.log(this.hiddenWord[i])
+                this.hiddenWord[i] = this.word.charAt(i);
         }
     },
-    setword: function(){
+    setword: function () {
         this.word = this.wordList[Math.floor(Math.random() * this.wordList.length)];
     },
-    guess: function(c){
+    guess: function (c) {
+        if (/[a-z]/i.test(c) == false) {
+            alert("Please enter a valid character from a-z.")
+            return;
+        }
+        else if (this.guessedLetters.indexOf(c.toLowerCase()) != -1) {
+            alert("Please guess a letter you haven't already guessed.")
+            return;
+        }
         var wordCorrect = [];
-        for (var i=0;i<this.word.length;i++){
-            if(/[a-z]/i.test(c)==false){
-                alert("Please enter a valid character from a-z.")
-                return;
-            }
-            else if(this.word[i].toLowerCase()==c.toLowerCase()){
+        for (var i = 0; i < this.word.length; i++) {
+
+            if (this.word[i].toLowerCase() == c.toLowerCase()) {
                 wordCorrect.push(i);
             }
         }
         this.guessedLetters.push(c);
-        if (wordCorrect.length == 0){
+        if (wordCorrect.length == 0) {
             this.guesses--;
-        }
-        else {
-            for (var j=0;j<wordCorrect.length;j++){
-                this.hiddenWord[wordCorrect[j]] = this.word.charAt(wordCorrect[j]);
+            if (this.guesses == 0) {
+                alert("You ran out of guesses. The correct answer was " + this.word);
+                this.new();
             }
         }
-        console.log(this.hiddenWord);
+        else {
+            for (var j = 0; j < wordCorrect.length; j++) {
+                this.hiddenWord[wordCorrect[j]] = this.word.charAt(wordCorrect[j]);
+            }
+            if (this.hiddenWord.indexOf("_") == -1) {
+                this.display();
+                console.log(this.hiddenWord);
+                console.log(this.gameword);
+                setTimeout(this.win,200);
+            }
+        }
+        this.display();
     },
-    win: function(){
+    win: function () {
         this.wins++;
+        alert("You won!");
         this.new();
     },
-    new: function(){
-        this.guesses=10;
-        this.guessedLetters = {};
+    new: function () {
+        this.guesses = 6;
+        this.guessedLetters = [];
+        this.hiddenWord = [];
         this.setword();
         this.hiddenword();
         this.display();
     },
-    display: function(){
-        gameword = '';
-        for (var i=0;i<this.hiddenWord.length;i++){
-        this.gameword+=this.hiddenWord[i];}
-        document.getElementById("word").innerHTML=this.gameword;
-        document.getElementById("guesses").innerHTML=this.guesses;
-        document.getElementById("wins").innerHTML=this.wins;
-        document.getElementById("guessed").innerHTML=this.guessedLetters;
+    display: function () {
+        this.gameword = '';
+        for (var i = 0; i < this.hiddenWord.length; i++) {
+            this.gameword += this.hiddenWord[i];
+        }
+        document.getElementById("word").innerHTML = this.gameword;
+        document.getElementById("guesses").innerHTML = this.guesses;
+        document.getElementById("wins").innerHTML = this.wins;
+        document.getElementById("guessed").innerHTML = this.guessedLetters;
     }
 }
 wordGame.new();
+document.onkeyup = function (event) {
+    var guess = event.key;
+    wordGame.guess(guess);
+    
+}
